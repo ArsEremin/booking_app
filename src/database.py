3ff6ee35f15cfda9ca8c549b5b5270f81da568c4
ars.eremin <ars.eremin@gmail.com>
2025-01-14 12:27:28 +0300

    added config

---
+++ src/database.py
@@ -0,0 +1,11 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+
+from config import settings
+
+engine = create_async_engine(settings.get_database_uri)
+
+async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+class Base(DeclarativeBase):
+    pass
